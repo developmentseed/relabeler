9bf2f6fe9317c71e74dae9142cc1b951efa6a9fa
Drew Bollinger <drew@developmentseed.org>
2018-01-20 09:02:36 -0500

    Convert back to npz

---
+++ to-npz.py
@@ -0,0 +1,6 @@
+import numpy as np
+import json
+
+geo = json.load(open('labels-modified.geojson', 'r'))
+tiles = { f['properties']['tile']: f['properties']['label'] for f in geo['features']}
+np.savez('labels.npz', **tiles)
