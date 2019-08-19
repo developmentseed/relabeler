203dd1acccd8871f37794f7f4e551e8af90689db
Rub21 <rub2106@gmail.com>
2019-08-19 14:21:25 -0500

    refactoring files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'typeface-roboto'; // eslint-disable-line
 import { Provider } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+import Map from './components/Map';
+import Header from './components/Header';
+import Control from './components/Control';
+
 import createStore from './store/store';
-import Container from './components/Container';
 
-const store = createStore;
-// eslint-disable-next-line
-window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  mainContainer: {
+    height: 'calc(100vh - 64px)'
+  }
+});
 
-const App = () => (
-  <Provider store={store}>
-    <Container />
-  </Provider>
-);
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <Header />
+        <Grid container spacing={0} className={classes.mainContainer}>
+          <Control />
+          <Grid item xs={12} sm={12}>
+            <Map />
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(App);
---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1
+  },
+  sidebar: {
+    color: theme.palette.text.secondary,
+    display: 'flex',
+    flexFlow: 'column',
+    height: '100%'
+  },
+  title: {
+    color: '#fff'
+  },
+  toolbar: {
+    background: '#534e4e'
+  }
+});
+
+export const Header = props => {
+  const { classes } = props;
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Relabeler
+          <Typography variant="caption" className={classes.title}>
+            Update Machine Learning Labels
+          </Typography>
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Header);
